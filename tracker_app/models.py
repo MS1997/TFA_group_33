20a775a45a0a3f46578233cdcccf702a61a64a1c
Soumya Kumar <sk4679@columbia.edu>
2019-11-30 21:26:41 +0000

    Innititate model

--- tracker_app/models.py
+++ tracker_app/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+ from django.db import models
+  
+ from django.utils.translation import gettext as _
+ 
+ class squirrel(models.Model):
+     X = models.FloatField(
+         max_length=100,
+         help_text=_('Longitide Coordinate for Squirrel sighting point'),
+                            
 
-# Create your models here.
