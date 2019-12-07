316a4d23d3c1ecdd70cba554fcc46b647208000d
Soumya Kumar <sk4679@columbia.edu>
2019-12-07 02:00:57 +0000

    Added stats view

--- tracker_app/models.py
+++ tracker_app/models.py
@@ -5,14 +5,12 @@ class Squirrel(models.Model):
      X = models.FloatField(
          max_length=100,
          help_text=_('Longitude Coordinate for Squirrel sighting point'),
-         decimal_places = 15,
          blank = True,
          )
 
      Y = models.FloatField(
          max_length=100,
          help_text=_('Latitude Coordinate for Squirrel sighting point'),
-         decimal_place = 15,
          blank = True,
          )
      UID= models.CharField(
@@ -29,7 +27,7 @@ class Squirrel(models.Model):
      Shift  = models.CharField(
          max_length=100,
          choices = SHIFT_CHOICES,
-         help_text=_('Squirrel sighting session morning or late afternoon') ,
+         help_text=_('Squirrel sighting session morning or late afternoon'),
          blank = True,
          )
      Date = models.IntegerField(
@@ -111,8 +109,7 @@ class Squirrel(models.Model):
      Kuks = models.BooleanField(
             max_length = 10,
             help_text=_('Squirrel was heard kukking, a chirpy vocal communication'),
-         
-            dafault = False,
+            default = False,
             )
      Quaas = models.BooleanField(
             max_length = 10,
--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns =[
              path('sightings/<str:UID>/',views.edit_squirrel),
-             path('sightings/add',views.add_squirrel),
+             path('sightings/add/',views.add_squirrel),
              path('map/',views.map_squirrel),
              path('sightings/',views.sighting),
              ]
