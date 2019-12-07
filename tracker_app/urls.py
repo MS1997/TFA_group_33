a6bfdd8c353736b3a2a2ad950f11a7e03ac279b7
Metika Sikka <33345903+MS1997@users.noreply.github.com>
2019-12-07 01:45:19 -0500

    update urls.py

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -3,14 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-<<<<<<< HEAD
              path('sightings/<str:UID>',views.edit_squirrel),
              path('sightings/add/',views.add_squirrel),
              path('sightings/stats/',views.get_stats),
-=======
-             path('sightings/<str:UID>/',views.edit_squirrel),
-             path('sightings/add/',views.add_squirrel),
->>>>>>> 2bbb717e1f5bf24e732d883cf23b4b0ce995b347
              path('map/',views.map_squirrel),
              path('sightings/',views.sighting),
              ]
